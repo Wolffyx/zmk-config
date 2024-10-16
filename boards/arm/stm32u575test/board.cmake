e3f38166b072ac100083acfdc0960170ccedd921
Wolffyx <wolffdev@gmail.com>
2024-10-16 21:35:42 +0300

    Test 11

--- boards/arm/stm32u575test/board.cmake
+++ boards/arm/stm32u575test/board.cmake
@@ -1,10 +1,7 @@
 # set(BOARD_FLASH_RUNNER stm32cubeprog)
 
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
-board_runner_args(jlink "--device=STM32U575xx" "--speed=4000")
 
-include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 board_runner_args(stm32cubeprogrammer "--erase" "--port=swd" "--reset-mode=hw")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
@@ -14,8 +11,10 @@ board_runner_args(openocd "--no-halt")
 
 board_runner_args(pyocd "--target=stm32u575vgt6")
 
+board_runner_args(jlink "--device=STM32U575xx" "--speed=4000")
 board_runner_args(jlink "--device=STM32U575VG" "--reset-after-load")
 
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
